403175841058838c7384aac08b141e9a29446c24
mayank.sharma <mayank.sharma@spectator-video.com>
2024-02-23 14:16:39 +0100

    Enabled xamarin login for google

---
+++ HealthPrototype/HealthPrototype/Model/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Auth;
+
+namespace HealthPrototype.Model
+{
+    public class Constants
+    {
+
+        public static string AppName = "HealthPrototype";
+
+        // Google Fit REST API parameters for Xamarin Auth
+        public static string CLIENT_ID = "1093429039004-kal5om4tip1v1svbi259j60kr2crfn0p.apps.googleusercontent.com";
+        public static string CLIENT_SECRET = null;
+        public static string AUTHORIZE_URL = "https://accounts.google.com/o/oauth2/auth";
+        public static string REDIRECT_TOKEN_URL = "com.bytesensei.healthprototype://auth";
+        public static string ACCESS_TOKEN_URL = "https://oauth2.googleapis.com/token";
+        public static GetUsernameAsyncFunc GET_USERNAME_ASYNC = null;
+        public static bool IS_USING_NATIVE_UI = true;
+
+        // Scopes for Google Fit REST API
+        public static string FITNESS_ACTIVITY_READ = "https://www.googleapis.com/auth/fitness.activity.read";
+
+    }
+}
--- HealthPrototype/HealthPrototype/View/MainPage.xaml.cs
+++ HealthPrototype/HealthPrototype/View/MainPage.xaml.cs
@@ -1,18 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xamarin.Auth;
+using Xamarin.Auth.Presenters;
 using Xamarin.Forms;
+using HealthPrototype.Model;
+using System.Diagnostics;
 
 namespace HealthPrototype
 {
     public partial class MainPage : ContentPage
     {
+
+        private OAuth2Authenticator authenticator;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Entered!");
+
+            OAuth2Authenticator authenticator = new OAuth2Authenticator(
+                clientId: Constants.CLIENT_ID,
+                clientSecret: Constants.CLIENT_SECRET,
+                scope: Constants.FITNESS_ACTIVITY_READ,
+                authorizeUrl: new Uri(Constants.AUTHORIZE_URL),
+                redirectUrl: new Uri(Constants.REDIRECT_TOKEN_URL),
+                accessTokenUrl: new Uri(Constants.ACCESS_TOKEN_URL),
+                getUsernameAsync: Constants.GET_USERNAME_ASYNC,
+                isUsingNativeUI: Constants.IS_USING_NATIVE_UI
+            );
+
+            authenticator.Completed += OnAuthenticationCompleted;
+            authenticator.Error += OnAuthenticationError;
+
+            Console.WriteLine("Exited!");
+
+            var presenter = new OAuthLoginPresenter();
+            presenter.Login(authenticator);
+        }
+
+        private async void OnAuthenticationCompleted(object sender, AuthenticatorCompletedEventArgs e)
+        {
+
+            if (e.IsAuthenticated)
+            {
+                string accessToken = e.Account.Properties["access_token"];
+                await DisplayAlert("Success", accessToken, "OK");
+               
+            }
+            else
+            {
+                await DisplayAlert("Error", "Authentication failed", "OK");
+            }
+        }
+
+        private async void OnAuthenticationError(object sender, AuthenticatorErrorEventArgs e)
+        {
+            await DisplayAlert("Error", "Authentication error: " + e.Message, "OK");
         }
     }
 }
